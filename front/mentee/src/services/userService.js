74cf74741d61b5099a7c4c9a3074d9e3ce2cb71b
shamil-b <shamilbedru47@gmail.com>
2023-09-10 12:04:14 +0300

    refactor homepage api calls to classes service

---
+++ front/mentee/src/services/userService.js
@@ -0,0 +1,17 @@
+import {localIp} from "../constants";
+
+
+export async function getMe() {
+    return await fetch(`${localIp}/users/me`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": localStorage.getItem("token")
+        }
+    })
+        .then((res) => res.json())
+        .then((data) => data)
+        .catch((err) => {
+            console.log(err);
+        });
+}
