475cf16879c710186ca1bdc03cb385494bd3bc01
shamil-b <shamilbedru47@gmail.com>
2023-09-10 12:13:32 +0300

    refactor dashboard api calls to classes service

--- front/mentee/src/screens/dashboard.js
+++ front/mentee/src/screens/dashboard.js
@@ -4,6 +4,8 @@ import Footer from "../components/footer";
 import Profile from "../components/profile";
 import MenteeHeader from "../components/mentee_header";
 import { localIp } from "../constants";
+import {getAllClasses, searchClasses as searchClassesService} from "../services/classesService";
+import {getMe} from "../services/userService";
 
 export default function Dashboard() {
   const [allLectures, setAllLectures] = useState(null);
@@ -13,12 +15,24 @@ export default function Dashboard() {
   const [subscribed, setSubscribed] = useState(true);
   const [created, setCreated] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
+  const [errorMessage, setErrorMessage] = useState("");
 
   const [totalLectures, setTotalLectures] = useState(0);
   const [pageNumbers, setPageNumbers] = useState([]);
   const lecturesPerPage = 1;
 
-   void function generatePageNumbers(start){
+    const searchClasses = async (term)=> {
+        try {
+            const classes = await searchClassesService(term);
+            setAllLectures(classes);
+        }
+        catch (e){
+            setErrorMessage(e.message.toString());
+        }
+    }
+
+
+    void function generatePageNumbers(start){
     let curPageNumbers = [];
     for (let i = start; i <= Math.ceil(totalLectures / lecturesPerPage); i++) {
         curPageNumbers.push(i);
@@ -31,38 +45,19 @@ export default function Dashboard() {
 
   useEffect(() => {
     // we should fetch for both subscribed and created lectures and set their corresponding states
-    fetch(localIp + "/classes", {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": localStorage.getItem("token")
-        }
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        setUpcomingLectures(data);
-        setAllLectures(data);
-        setTotalLectures(data.length);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+  getAllClasses().then((classes)=>{
+      setUpcomingLectures(classes);
+      setAllLectures(classes);
+      setTotalLectures(classes.length);
+  }).catch((e)=>{
+      console.log(e);
+  })
 
-    fetch(localIp + "/users/me", {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": localStorage.getItem("token")
-      }
-    })
-        .then((res) => res.json())
-        .then((data) => {
-          console.log(data);
-          setProfileInfo(data);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
+  getMe().then(userInfo=>{
+      setProfileInfo(userInfo);
+  }).catch((e)=>{
+      console.log(e);
+  })
   }, []);
 
   function toUpcoming() {
@@ -78,7 +73,7 @@ export default function Dashboard() {
 
   return (
     <div className="bg-gray-200 min-h-screen">
-      <MenteeHeader search={false} profileInfo={profileInfo}/>
+      <MenteeHeader search={true} onSearch={searchClasses} profileInfo={profileInfo}/>
       <div className="courses mt-4 ">
         <div className="flex flex-col gap-2">
           <div className="title text-2xl font-semibold text-gray-700 px-12 py-8 flex gap-1 justify-center">
@@ -155,17 +150,3 @@ export default function Dashboard() {
     </div>
   );
 }
-
-// dashboard contents for student
-// - completed lectures
-// - subscribed/upcoming lectures
-
-// dashboard contents for teacher
-// - created/upcoming lectures
-// - completed lectures
-
-// common contents
-// a side bar for navigation
-// navigation to:
-// - account settings
-// - logout
